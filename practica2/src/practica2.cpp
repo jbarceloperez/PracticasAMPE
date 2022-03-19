9cac95f26700bfef08ba0a382ea7d55656556c1c
jbarceloperez <javibarpe@hotmail.com>
2022-03-19 19:53:34 +0100

    comitting practica 2

--- practica2/src/practica2.cpp
+++ practica2/src/practica2.cpp
@@ -2,22 +2,50 @@
 #include <iostream>
 #include <immintrin.h>
 
-void practica2Linear(float* MK_matrix, float* KN_matrix, float* output_matrix, int M, int K, int N) {
-     std::cout << "Running the code for optimized matrix multiplication" << std::endl;
-     for(int i=0; i<M; i++) {
-         for(int j=0; j<N; j++) {
-	     float suma=0.0;
-             for(int k=0; k<K; k++) {
-	         suma+=MK_matrix[i*K+k]*KN_matrix[j*K+k];
-	     }
+#define VECTOR_SIZE 8
 
-	     output_matrix[i*N+j]=suma;
-         }
-     }
+void practica2Linear(float* MK_matrix, float* KN_matrix, float* output_matrix, int M, int K, int N) {
+    std::cout << "Running the code for optimized AVX matrix multiplication" << std::endl;
+    __m256 v1, v2, v3, aux; // vectores para calcular la multiplicación de matrices
+
+    for(int i=0; i<M; i++) {    // la última iteración se trata de forma diferente
+        // v1 = _mm256_set_ps(MK_matrix[0*i],MK_matrix[1*i],MK_matrix[2*i],MK_matrix[3*i],MK_matrix[4*i],MK_matrix[5*i],MK_matrix[6*i],MK_matrix[7*i]);
+        v1 = _mm256_load_ps(&MK_matrix[i*VECTOR_SIZE]);
+        for(int j=0; j<N/VECTOR_SIZE; j++) {    // la última iteración se trata de forma diferente
+            v3 = _mm256_setzero_ps();
+	        // v2 = _mm256_set_ps(KN_matrix[0*j],KN_matrix[1*j],KN_matrix[2*j],KN_matrix[3*j],KN_matrix[4*j],KN_matrix[5*j],KN_matrix[6*j],KN_matrix[7*j]);
+            v2 = _mm256_load_ps(&KN_matrix[j*VECTOR_SIZE]);
+            for(int k=0; k<K; k++) {
+                aux = _mm256_mul_ps(v1,v2);
+                v3 = _mm256_add_ps(v3,aux);
+	        }
+        }
+        if (K%VECTOR_SIZE == 0)   // se itera normal
+        {
+            v3 = _mm256_setzero_ps();
+	        // v2 = _mm256_set_ps(KN_matrix[0*j],KN_matrix[1*j],KN_matrix[2*j],KN_matrix[3*j],KN_matrix[4*j],KN_matrix[5*j],KN_matrix[6*j],KN_matrix[7*j]);
+            v2 = _mm256_load_ps(&KN_matrix[(N-1)*VECTOR_SIZE]);
+            for(int k=0; k<K; k++) {
+                aux = _mm256_mul_ps(v1,v2);
+                v3 = _mm256_add_ps(v3,aux);
+	        }
+        }
+        else 
+        {
+            int r = K%VECTOR_SIZE;
+            float *resto = (float *) malloc(VECTOR_SIZE*sizeof(float));
+            int j;
+            for (j=0;j<VECTOR_SIZE; j++)
+            {
+                if
+            }
+            free(resto);
+        }
+    }
 }
 
 void practica2LinearSeq(float* MK_matrix, float* KN_matrix, float* output_matrix, int M, int K, int N) {
-     std::cout << "Running the code for matrix multiplication" << std::endl;
+     std::cout << "Running the code for sequencial matrix multiplication" << std::endl;
      for(int i=0; i<M; i++) {
          for(int j=0; j<N; j++) {
              float suma=0.0;
--- tests/hello_avx.c
+++ tests/hello_avx.c
@@ -4,8 +4,15 @@
 int main() {
 
     /* Initialize the two argument vectors */
-    __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
+    __m256 evens = _mm256_set_ps(8.0, 5.0, 3.0, 6.0, 1.0, 8.0, 2.0, 3.0);
     __m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+
+    float *e = (float *)&evens;
+    float *o = (float *)&odds;
+    printf("\t%f %f %f %f %f %f %f %f\n  -\t",
+        e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7]);
+    printf("%f %f %f %f %f %f %f %f\n\t________________________________________________________________________________\n\t",
+        o[0], o[1], o[2], o[3], o[4], o[5], o[6], o[7]);
 
     /* Compute the difference between the two vectors */
     __m256 result = _mm256_sub_ps(evens, odds);
